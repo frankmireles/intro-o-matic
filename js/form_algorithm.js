0d5cc148ae27bb6f95919891e8b6de7503371b05
Francisco Mireles <franciscomireles7@gmail.com>
2022-02-16 01:47:49 -0600

    cambios segpun feedback para versión 1.1

--- js/form_algorithm.js
+++ js/form_algorithm.js
@@ -5,12 +5,12 @@
 3 - Benefit
 4 - Closing
 */
-var data = ['a', 'b', 'c', 'd', 'e'];
+var data = ['', '', '', '', ''];
 var adds = 0;
 
 function keepOnly1Checked(lastClicked, notLast){
-  var first = document.getElementById(lastClicked);
-  var second = document.getElementById(notLast);
+  let first = document.getElementById(lastClicked);
+  let second = document.getElementById(notLast);
   if (first.checked==true){
     second.checked=false;
   }
@@ -24,78 +24,58 @@ function hide(section){
   document.getElementById(section).style.display = "none";
 }
 
-function capture(input, preview, index, prefix){
-  var textField = document.getElementById(input).value;
-  var previewText = document.getElementById(preview);
+function capture(input, index, prefix){
+  let textField = document.getElementById(input).value;
   data[index] = (prefix + textField);
-  textField.value = "";
-  previewText.innerHTML = data[index];
-  window.alert("Dato capturado, para cambiar lo ingresado solo vuelva a capturar");
+  recalculate();
 }
 
 function captureConcept(){
-  var concepto = document.getElementById("concepto").value;
-  var adjConcepto = document.getElementById("adjConcepto").value;
-  var unionConcepto = document.getElementById("unionConcepto").value;
-  var razConcepto = document.getElementById("razConcepto").value;
-  var previewText = document.getElementById("q6-ap");
+  let concepto = document.getElementById("concepto").value;
+  let adjConcepto = document.getElementById("adjConcepto").value;
+  let unionConcepto = document.getElementById("unionConcepto").value;
+  let razConcepto = document.getElementById("razConcepto").value;
   data[1] = (concepto + " es " + adjConcepto + unionConcepto + razConcepto);
-  previewText.innerHTML = data[1];
-  window.alert("Dato capturado, para cambiar lo ingresado solo vuelva a capturar");
+  recalculate();
 }
 
 function captureStarter(){
-  var starter1 = document.getElementById("starter1").value;
-  var starter2 = document.getElementById("starter2").value;
-  var starter3 = document.getElementById("starter3").value;
-  var previewText = document.getElementById("q7p");
-  data[2] = starter1 + starter2 + starter3;
-  previewText.innerHTML = data[2];
-  window.alert("Dato capturado, para cambiar lo ingresado solo vuelva a capturar");
-}
-
-function addStarter(){
-  const joins = [' Además ', ' También ', ' Después ', ' Para finalizar '];
-  if (data[2] == ''){
-    window.alert("Si es el primer concepto presione Capturar");
+  const joins = [' Además, ', ' También, ', ' Después, ', ' Para finalizar, '];
+  let starter1 = document.getElementById("starter1").value;
+  let starter2 = document.getElementById("starter2").value;
+  let starter3 = document.getElementById("starter3").value;
+  if (adds === 0) {
+    data[2] = starter1 + starter2 + starter3;
+    adds++;
+  } else if (adds <= 4) {
+    data[2] += joins[adds-1] + starter2 + starter3;
+    adds++;
   } else {
-    if (adds < 4){
-      var starter2 = document.getElementById("starter2").value;
-      var starter3 = document.getElementById("starter3").value;
-      var previewText = document.getElementById("q7p");
-      data[2] = data[2] + joins[adds] + starter2 + starter3;
-      previewText.innerHTML = data[2];
-      window.alert("Dato capturado, para cambiar lo ingresado solo vuelva a capturar");
-      adds++;
-    } else {
-      window.alert("Solo se permite ingresar hasta 5 conceptos. Presiona clear para borrar todo.");
-    }
+    window.alert("Solo se permite ingresar hasta 5 conceptos. Presiona clear para borrar todo.");
   }
+  recalculate();
 }
 
 function clearStarter(){
-  var previewText = document.getElementById("q7p");
   data[2] = "";
   adds = 0;
-  previewText.innerHTML = data[2];
-  window.alert("Enunciado inicial borrado.");
+  recalculate();
 }
 
 function captureBenefit(){
-  var benefit1 = document.getElementById("benefit1").value;
-  var benefit2 = document.getElementById("benefit2").value;
-  var benefit3 = document.getElementById("benefit3").value;
-  var previewText = document.getElementById("q9p");
-  data[3] = benefit1 + " te " + benefit2 + " para " + benefit3;
-  previewText.innerHTML = data[3];
-  window.alert("Dato capturado, para cambiar lo ingresado solo vuelva a capturar");
+  let benefit1 = document.getElementById("benefit1").value;
+  let benefit2 = document.getElementById("benefit2").value;
+  let benefit3 = document.getElementById("benefit3").value;
+  data[3] = benefit1 + " te " + benefit2 + benefit3;
+  recalculate();
 }
 
 function recalculate(){
-  var finalOutput = document.getElementById("finalOutput");
-  var finalIntro = "";
+  let finalOutput = document.getElementById("finalOutput");
+  let finalIntro = "";
   for (var i = 0; i < data.length; i++) {
     finalIntro += " " + data[i];
   }
   finalOutput.value = finalIntro;
+  //window.scrollTo(0,document.body.scrollHeight);
 }
